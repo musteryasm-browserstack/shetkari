8dd50e0621ecdd225aef718f24c3fe0684dbfad4
Hardik Shah <hardikts@gmail.com>
2025-03-28 11:47:34 +0530

    adding link to footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SIDEBAR_LINKS } from "@/lib/constants";
 
 export function Footer() {
@@ -5,9 +6,9 @@ export function Footer() {
         <>
             <footer className="flex flex-row gap-2 items-center justify-evenly fixed bottom-0 bg-muted rounded-2xl p-2 z-50 sm:hidden w-screen py-4">
                 {SIDEBAR_LINKS.map((link, index) => (
-                    <div key={index} className="p-2 rounded-xl bg-primary/20">
+                    <Link href={link.url} key={index} className="p-2 rounded-xl bg-primary/20">
                         <link.icon className="w-4 h-4" />
-                    </div>
+                    </Link>
                 ))}
             </footer>
         </>
